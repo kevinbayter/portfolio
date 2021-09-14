9721dfe4f0837dec85db283f9c0389d2475116ee
Kevin Bayter <kevinbayter@gmail.com>
2021-09-14 12:18:28 -0500

    Se hace funcional el contact form

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -92,7 +92,7 @@ jQuery(document).ready(function($) {
     else var str = $(this).serialize();
     var action = $(this).attr('action');
     if( ! action ) {
-      action = 'contactform/contactform.php';
+      action = 'contactform../index.html';
     }
     $.ajax({
       type: "POST",
@@ -116,3 +116,4 @@ jQuery(document).ready(function($) {
   });
 
 });
+
